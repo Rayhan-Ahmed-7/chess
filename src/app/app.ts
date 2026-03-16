6d17a9f6313a5206efa3ba977c3cac616e7f74e9
Rayhan Ahmed <rayhan.ahmed@ibos.io>
2026-03-16 15:04:55 +0600

    feat: Initialize the Angular root component (`App`) with its template, basic styling, and `InputComponent` import.

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { InputComponent } from './shared/ui/input/input.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, InputComponent],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
